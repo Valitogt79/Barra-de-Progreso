9c61f97c608daa7080430f41105f61fdeafb4ad1
Oswaldo Alonzo <oalonzo@gmail.com>
2021-07-27 20:56:25 -0600

    terminando programacion de script.js y cambio al css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // obtener los pasos (steps)
-const steps = document.querySelectorAll(".steps");
+const steps = document.querySelectorAll(".step");
 
 //obtener la barra de progreso
 const bar = document.getElementById("bar");
@@ -14,11 +14,42 @@ let currentStep = 1;
 //escuchando el botón siguiente
 nextButton.addEventListener("click", () => {
   //sumar 1 a los pasos
-  currentStep++;
+  if (currentStep < steps.length) {
+    currentStep++;
+    updateProgress();
+  }
+});
+
+//escuchar el boton anterior
+prevButton.addEventListener("click", () => {
+  if (currentStep > 1) {
+    //sumar 1 a los pasos
+    currentStep--;
+    updateProgress();
+  }
+});
+
+//funcion para actualizar el progreso
+function updateProgress() {
+  //actualizar pasos
   steps.forEach((step, index) => {
-    console.log(index);
     if (index < currentStep) {
       step.classList.add("active");
+    } else {
+      step.classList.remove("active");
     }
   });
-});
+
+  //actualizar barra
+  const progress = ((currentStep - 1) / (steps.length - 1)) * 100;
+  bar.style.width = progress + "%";
+
+  // control de los botones
+  if (currentStep === steps.length) {
+    nextButton.disabled = true;
+  } else if (currentStep === 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = nextButton.disabled = false;
+  }
+}
